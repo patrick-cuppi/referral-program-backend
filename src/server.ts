fe3a2a671f458c514e51e87053dedbabfb05749d
Patrick-Cuppi <cuppi.patrick@gmail.com>
2025-02-20 15:56:56 -0300

    raw: config cors

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,17 @@
 import { app } from "./app";
+import { fastifyCors } from "@fastify/cors"
+import { 
+    validatorCompiler, 
+    serializerCompiler 
+} from "fastify-type-provider-zod"
+
+app.setSerializerCompiler(serializerCompiler)
+app.setValidatorCompiler(validatorCompiler)
+
+app.register(fastifyCors, {
+    origin: true, // Production example: 'http://localhost:3000'
+
+})
 
 app.listen({ port: 3333 }).then(() => {
     console.log('👽 HTTP Server Running!')
